2dc1afad48cd8d7a8e0b52b48eb240c97ef85897
abiud45 <muriithiabiud45@gmail.com>
2026-08-03 15:51:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1957,6 +1957,13 @@ with app.app_context():
 with app.app_context():
     db.create_all()
 
+with app.app_context():
+    db.create_all()
+
+    print("Eggs:", Egg.query.count())
+    print("Chicks:", ChickBatch.query.count())
+    print("Feeds:", Feed.query.count())    
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
